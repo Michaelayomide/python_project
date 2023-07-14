ae4df6cb7051c8265d2d384e1abf7d57b056047c
Eto David <etodavid70@gmail.com>
2023-07-14 16:28:54 +0100

    my first change

---
+++ dic.py
@@ -0,0 +1,5 @@
+name = {
+    "x":"ayo",
+    "y":"tobi",
+}
+print(name)
---
+++ index.py
@@ -0,0 +1,56 @@
+# y=2.8
+# print(type(y))
+# y=int(2.8)
+# print(type(y))
+
+# a="hello "
+# b="world"
+# c= a + b
+# print(c)
+
+# age = 36
+# txt = "My name is John, and I am {}"
+# print(txt.format(age))
+
+# a=30
+# b=20
+
+# if b!=a:
+#     print("bad")
+# elif b < a:
+#     print("very good")    
+# else:
+#     print("good")    
+
+
+# x = "Hello"
+# y = 15
+
+# print(bool(False))
+# print(bool(y))
+# a="hello"
+# print(a.upper())
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i -= 1
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#function
+
+def my_function():
+  print("Hello from a function")
+my_function()
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+
